d9b5c742377484476c189d381eff859aa0498f37
KentonDunn <kenton.dunn@gmail.com>
2018-04-13 23:02:15 -0400

    added a hide/show feature to display results.  still need to work on the answer logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+/* need logic to capture the answers and calculate correct and incorrect answers */
+
+/* Create Correct answer array and Incorrect answer array
+
+Unanswered is either an array or a response
+
+Create var answerKey with the correct answers
+
+Use the O5-Timers Activity #15 File Extension exercise to create the formula to calculate correct and incorrect answers*/
+
+function check(){
+    console.log("linked");
+    $(".results").show();
+    $(".quiz").hide(1500);
+    $(".results").css("visibility","visible");
+
+}
+//begin to collect the answers and code it.  
+var question1 = this.question1.value;
+console.log(question1.value);
